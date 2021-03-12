21259d168fd0db8cc2f0aca278c34648cf478c92
wilcox marin <haiber.nino@ciberdix.com>
2021-03-12 12:25:37 -0500

    archivos basico para iniciar proyecto en node js

---
+++ setup_node_ts/src/App.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+const app = express()
+const port = 5000
+app.get('/', (_, res) => {
+    res.status(200).send('Hola mundo')
+})
+app.listen(port, () => console.log(`Running on port ${port}`))
